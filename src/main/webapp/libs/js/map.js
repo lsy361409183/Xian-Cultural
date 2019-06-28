3ec58976f3bc6ec0a4c1fc105756bd9c991e4bea
wl <wanglei@yeyisun.com>
2019-06-28 11:00:38 +0800

    add location data

--- src/main/webapp/libs/js/map.js
+++ src/main/webapp/libs/js/map.js
@@ -1,6 +1,6 @@
 var cl_map = new AMap.Map("wl_map_warp",{
     zoom:11,//级别
-    center: [116.397428, 39.90923],//中心点坐标
+    center: [108.947016,34.270124],//中心点坐标
     // viewMode:'3D'//使用3D视图
 });
 
@@ -17,37 +17,45 @@ $(window).resize(function(){
 })
 
 
-var layer = new Loca.PointLayer({
-    map: cl_map
-});
-// $.ajax({
-//     type:"get",
-//     url:"/getData",
-//     data:{"unit": department, "code": centerNum},
-//     dataType:"json",
-//     success:function(res){
-//         if(res.code==0){
-          
-//         }else{
-            
-//         }
-//     }
-// });
-var data = [
-    { "lnglat": [116.366794, 39.915309], "name": "西城区", "style": 2 }, 
-    { "lnglat": [116.486409, 39.921489], "name": "朝阳区", "style": 2 }, 
-    { "lnglat": [116.286968, 39.863642], "name": "丰台区", "style": 2 },
-]
-layer.setData(data, {
-    lnglat: 'lnglat'
-});
-layer.setOptions({
-    style: {
-        radius: 5,
-        color: '#9999ff',
-        borderColor: '#5a6aff',
-        borderWidth: 1,
-        opacity: 0.9,
-    }
-});
-layer.render();
+
+function getData() {
+    $.ajax({
+        type:"get",
+        url:"/getdata",
+        dataType:"json",
+        success:function(res){
+           renderMap(res)
+        }
+    })
+}
+getData()
+function renderMap (data) {
+    var dataSource = [];
+    dataSource = data.map(function (item){
+        var tempArr = item.culturalCoordinate.split(",");
+        tempArr[0] = parseFloat(tempArr[0]);
+        tempArr[1] = parseFloat(tempArr[1]);
+        return {
+            lnglat: tempArr,
+            name: item.culturalName,
+        }
+    });
+    console.log("dataSource", dataSource)
+    var layer = new Loca.PointLayer({
+        map: cl_map
+    });
+    layer.setData(dataSource, {
+        lnglat: 'lnglat'
+    });
+    layer.setOptions({
+        style: {
+            radius: 5,
+            color: '#9999ff',
+            borderColor: '#5a6aff',
+            borderWidth: 1,
+            opacity: 0.9,
+        }
+    });
+    layer.render();
+}
+
